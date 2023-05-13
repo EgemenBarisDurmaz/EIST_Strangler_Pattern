c189636f5a8996ef61192482b7bdb694bdf0acc5
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-05-13 14:37:14 +0200

    to-do 1

--- obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
+++ obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
@@ -1,5 +1,7 @@
 package de.tum.in.ase.pse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,6 +9,10 @@ public class ObiWanKenobiController {
 
     // TODO 1.2 Implement an enpoint responsible for training new apprentices.
     //  It should operate under the same path as in MasterYodaController
+    @PostMapping("/train-apprentices")
+    public String trainApprentices() {
+        return "Apprentices are being trained...";
+    }
 
 
 }
