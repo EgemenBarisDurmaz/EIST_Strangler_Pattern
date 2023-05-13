a301f88e4b18da4c8753b6c0c9f793bf7b5f70ff
artemis <artemis@xcit.tum.de>
2023-05-13 14:29:55 +0200

    Reset Exercise

--- obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
+++ obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
@@ -1,14 +1,9 @@
 package de.tum.in.ase.pse.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ObiWanKenobiController {
-    @PostMapping("/train-apprentices")
-    public String trainApprentices() {
-        return "Apprentices are being trained...";
-    }
 
     // TODO 1.2 Implement an enpoint responsible for training new apprentices.
     //  It should operate under the same path as in MasterYodaController
